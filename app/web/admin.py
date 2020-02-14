eab05d6f0c8cba7a40d9a8cd6a7c048cd91f3d82
Mario Esteban Hernandez Hernandez <hdez.marioe@gmail.com>
2020-02-13 20:50:19 -0600

    registro de modelos en admin panel

--- app/web/admin.py
+++ app/web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app.web.models import Cliente, Orden, Subcription, Type_Payment, Tipo_subcripcion
+
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Orden)
+admin.site.register(Subcription)
+admin.site.register(Type_Payment)
+admin.site.register(Tipo_subcripcion)
